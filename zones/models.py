75372ac3a82726368cccd4004a1c9b94f9cdb1d7
Alibi <alibi.askar24@gmail.com>
2025-05-16 19:31:53 +0500

    Fix duplicate Room model

--- zones/models.py
+++ zones/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Room(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
-    capacity = models.PositiveIntegerField()
-    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='room_images/', null=True, blank=True)
-
-    def __str__(self):
-        return self.name
 
 class BathOrBBQZone(models.Model):
     ZONE_TYPES = [
