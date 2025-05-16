ac91863f4d01fe341cfdde8a116bea6cdc3ca45b
Alibi <alibi.askar24@gmail.com>
2025-05-16 18:40:26 +0500

    Удалена модель ZoneBooking и связанные импорты

--- booking/serializers.py
+++ booking/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import RoomBooking, ZoneBooking
+from .models import RoomBooking
 
 class RoomBookingSerializer(serializers.ModelSerializer):
     class Meta:
