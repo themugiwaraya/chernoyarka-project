f6330821527eb608f765bb2d67a3129296701d7e
Alibi <alibi.askar24@gmail.com>
2025-05-22 02:44:52 +0500

    add register for bath-bbq

--- booking/admin.py
+++ booking/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import RoomBooking
+from .models import RoomBooking, BathOrBBQZoneBooking  
 
 @admin.register(RoomBooking)
 class RoomBookingAdmin(admin.ModelAdmin):
     list_display = ('room', 'name', 'check_in', 'check_out', 'created_at')
     list_filter = ('room', 'check_in', 'check_out')
+    search_fields = ('name', 'phone', 'email')
+    ordering = ('-created_at',)
+
+@admin.register(BathOrBBQZoneBooking)
+class BathOrBBQZoneBookingAdmin(admin.ModelAdmin):
+    list_display = ('zone', 'name', 'booking_date', 'created_at')
+    list_filter = ('zone', 'booking_date')
     search_fields = ('name', 'phone', 'email')
     ordering = ('-created_at',)
